ea70abb6283c9a01f87420045583f0754aebb645
caapel <caapel@mail.ru>
2024-01-08 20:16:18 +0300

    Решена подзадача 1

--- ProjectForGit/Program.cs
+++ ProjectForGit/Program.cs
@@ -1,16 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProjectForGit
+﻿namespace ProjectForGit
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+        }
+
+        //решил подзадачу номер 1
+        private static int Y(int X)
+        {
+            return 2 * X - 5;
         }
     }
 }
